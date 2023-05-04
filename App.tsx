0ac729f7ff6bb55ca1a3f0b66f0e007462f71c1c
Jon Andoni Baranda <jbaranda@cccis.com>
2023-05-03 20:34:28 -0500

    Message UI started

--- App.tsx
+++ App.tsx
@@ -1,26 +1,57 @@
 import 'react-native-gesture-handler';
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import {View, StyleSheet, Text, SafeAreaView} from 'react-native';
 
 
-import Explore from './components/Explore'
-const Stack = createNativeStackNavigator();
+import HomeScreen from './src/screens/HomeScreen';
+import MatchesScreen from './src/screens/NewMatchesScreen';
+import ChatsScreen from './src/screens/ChatsScreen';
+import ChatScreen from './src/screens/ChatScreen';
 
-function App() {
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+
+
+const App = () => {
+  const color = "#b5b5b5";
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen 
-        name="Explore" 
-        component={Explore}
-        options={{
-          headerShown: false,
-        }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaView style={styles.root}>
+      <View style={styles.pageContainer}>
+        <View style={styles.topNavigation}>
+          <Fontisto name="tinder" size={30} color={color} />
+          <MaterialCommunityIcons name="star-four-points" size={30} color={color} />
+          <Ionicons name="ios-chatbubbles" size={30} color={color} />
+          <FontAwesome name="user" size={30} color={color} />
+        </View>
+      </View>
+      <ChatScreen/>
+    </SafeAreaView>
   );
-}
+};
 
-export default App;
+const styles = StyleSheet.create({
+
+
+  root: {
+    flex: 1,
+  },
+
+  pageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  topNavigation: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    padding: 10,
+
+  }
+
+  });
+
+  export default App;
--- assets/data/suggestedPlansData.js
+++ assets/data/suggestedPlansData.js
@@ -1,20 +1,24 @@
 const suggestedPlansData = [
     {
+        id: '1',
         title: 'GYM EVENT',
         image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png',
         description: "This is going to be the best sport event in the world.",
     },
     {
+        id: '2',
         title: 'Hawaii-an party',
         image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/jeff.jpeg',
         description: "This is going to be the best sport event in the world.",
     },
     {
+        id: '3',
         title: 'Hawaii-an party',
         image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg',
         description: "This is going to be the best sport event in the world.",
     },
     {
+        id: '4',
         title: 'Hawaii-an party',
         image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim1.JPG',
         description: "This is going to be the best sport event in the world.",
