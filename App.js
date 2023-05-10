09127f5f2c925f7a49528c63255b6996fb0328de
Jon Andoni Baranda <jbaranda@cccis.com>
2023-05-10 14:07:52 -0500

    Finished user authentication

--- App.js
+++ App.js
@@ -1,14 +1,49 @@
 import {View, StyleSheet} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import Navigator from './src/navigation';
-import { Amplify } from 'aws-amplify';
+import { Amplify, Auth, API, graphqlOperation} from 'aws-amplify';
 import awsconfig from './src/aws-exports';
 import {withAuthenticator} from 'aws-amplify-react-native';
+import { useEffect } from 'react';
+import { getUser } from './src/graphql/queries';
+import { createUser } from './src/graphql/mutations';
 
 Amplify.configure({...awsconfig, Analytics: { disabled: true } });
 
 
 function App() {
+
+  useEffect(() => {
+    const syncUser = async () => {
+      const authUser = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      });
+      console.log(authUser);
+
+      const userData = await API.graphql(
+        graphqlOperation(getUser, { id: authUser.attributes.sub})
+      );
+      console.log(userData);
+
+      if (userData.data.getUser) {
+        console.log("User already exists in DB");
+        return;
+      }
+
+      const newUser = {
+        id: authUser.attributes.sub,
+        name: authUser.attributes.phone_number,
+        status: 'Hey, I am using twosocial',
+      };
+
+      const newUserResponse = await API.graphql(
+        graphqlOperation(createUser, { input: newUser} )
+      );
+    };
+
+    syncUser();
+
+  }, []);
 
   return (
     <View style={styles.container}>
--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,11 +1,11 @@
-import { View, Text, Button } from 'react-native';
+import { View, Button } from 'react-native';
 import React from "react";
-import {Auth} from 'aws-amplify';
+import { Auth } from 'aws-amplify';
 
 const SettingsScreen = () => {
     return (
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Button onPress={Auth.signOut()} title='Sign out' />
+            <Button onPress={() => Auth.signOut()} title="Sign out" />
         </View>
     );
 };
